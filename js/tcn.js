01927eaea0a4a35d6cb0f2205febe3def34f605f
marmol <marmol@linux-qgtc.suse>
2015-12-09 15:31:02 +0100

    Ahora al testear un campo se lanzan eventos

--- js/tcn.js
+++ js/tcn.js
@@ -4,6 +4,61 @@
     throw new Error('No se ha encontrado jQuery');
   }
 
+  function fireEvent(node, eventName) {
+    // Make sure we use the ownerDocument from the provided node to avoid cross-window problems
+    var doc;
+    if (node.ownerDocument) {
+      doc = node.ownerDocument;
+    } else if (node.nodeType == 9) {
+      // the node may be the document itself, nodeType 9 = DOCUMENT_NODE
+      doc = node;
+    } else {
+      throw new Error("Invalid node passed to fireEvent: " + node.id);
+    }
+
+    if (node.dispatchEvent) {
+      // Gecko-style approach (now the standard) takes more work
+      var eventClass = "";
+
+      // Different events have different event classes.
+      // If this switch statement can't map an eventName to an eventClass,
+      // the event firing is going to fail.
+      switch (eventName) {
+        case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
+        case "mousedown":
+        case "mouseup":
+          eventClass = "MouseEvents";
+          break;
+
+        case "focus":
+        case "change":
+        case "blur":
+        case 'input':
+        case 'keyup':
+        case "select":
+        case 'propertychange':
+          eventClass = "HTMLEvents";
+          break;
+
+        default:
+          throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
+      }
+      var event = doc.createEvent(eventClass);
+
+      var bubbles = eventName == "change" ? false : true;
+      event.initEvent(eventName, bubbles, true); // All events created as bubbling and cancelable.
+
+      event.synthetic = true; // allow detection of synthetic events
+      // The second parameter says go ahead with the default action
+      node.dispatchEvent(event, true);
+    } else if (node.fireEvent) {
+      // IE-old school style
+      var evento = doc.createEventObject();
+      evento.synthetic = true; // allow detection of synthetic events
+      node.fireEvent("on" + eventName, evento);
+    }
+  }
+
   function esCampoValido($campo){
     var campo = $campo.get(0);
     //checkValidity returns a boolean
@@ -16,6 +71,12 @@
 
   function introducirYComprobar($campo, valor){
     $campo.val(valor);
+
+    //Simulamos que el usuario introduce los valores
+    ['input', 'change', 'click', 'keyup', 'propertychange'].forEach(function(nombreEvento){
+      fireEvent($campo.get(0), nombreEvento);
+    });
+
     return esCampoValido($campo);
   }
 
