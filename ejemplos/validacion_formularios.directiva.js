a0e32adbb981bbd7765124afb7ce2ce87b120ece
Martin <tm07081995@gmail.com>
2015-12-31 16:12:00 +0100

    Eliminada dependencia jQuery

--- ejemplos/validacion_formularios.directiva.js
+++ ejemplos/validacion_formularios.directiva.js
@@ -1,4 +1,4 @@
-(function($){
+(function(){
   function esNif(dni) {
     var numero, letr, letra, expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
     if (expresion_regular_dni.test(dni) === true) {
@@ -33,11 +33,12 @@
     _validar.call(campo, arguments);
   }
 
-  function contarCheckboxesSeleccionados($checkboxes){
-    var cnt = 0;
-    $checkboxes.each(function(i){
-      cnt += $(this).is(':checked') ? 1 : 0;
-    });
+  function contarCheckboxesSeleccionados(checkboxes){
+    var cnt = 0, i, e;
+    for (i = 0; i < checkboxes.length; i++){
+      e = checkboxes[i];
+      cnt += e.checked ? 1 : 0;
+    }
     return cnt;
   }
 
@@ -58,56 +59,60 @@
   }
 
   function validarCheckbox(campo){
-    var $campo = $(campo), nombreCheckbox, $checkboxes,
-      minValores, maxValores, numeroExactoValores, resultadoValido, $campoError, mensajeError;
+    var nombreCheckbox, checkboxes, e, i,
+      minValores, maxValores, numeroExactoValores, resultadoValido, campoError, mensajeError;
 
     //Comprobamos existencia del atributo for y del campo del for
-    nombreCheckbox = $campo.attr('for');
+    nombreCheckbox = campo.htmlFor;
     if (!nombreCheckbox) return;
-    $checkboxes = $('input:checkbox[name="' + nombreCheckbox + '"]');
-    if ($checkboxes.length > 0){
+    checkboxes = document.querySelectorAll('input[type="checkbox"][name="' + nombreCheckbox + '"]');
+    if (checkboxes.length > 0){
       //Valores min, max y numExacto
-      numeroExactoValores = parseInt($campo.attr('data-validar-checkbox'));
+      numeroExactoValores = parseInt(campo.dataset.validarCheckbox);
       if (isNaN(numeroExactoValores)){
         numeroExactoValores = null;
 
-        minValores = parseInt($campo.attr('data-validar-checkbox-min'));
+        minValores = parseInt(campo.dataset.validarCheckboxMin);
         if (isNaN(minValores)) minValores = null;
 
-        maxValores = parseInt($campo.attr('data-validar-checkbox-max'));
+        maxValores = parseInt(campo.dataset.validarCheckboxMax);
         if (isNaN(maxValores)) maxValores = null;
 
         if (!minValores && !maxValores) return;
       }
 
       //Campo Error
-      $campoError = $($campo.attr('data-validar-checkbox-campo-error'));
-      mensajeError = $campo.attr('data-validar-checkbox-mensaje-error');
+      campoError = document.querySelector(campo.dataset.validarCheckboxCampoError);
+      mensajeError = campo.dataset.validarCheckboxMensajeError;
       if (!mensajeError){
-        mensajeError = $campoError.length > 0 ? $.trim($campoError.text()) : 'Invalid checks number';//TODO: mejorar
+        mensajeError = campoError ? campoError.innerText.trim() : 'Invalid checks number';//TODO: mejorar
       }
 
-
       //Escuchamos cambios en los checkboxes
-      $checkboxes.change(function(){
-        //Comprueba si el número de checkboxes seleccionados es correcto
-        resultadoValido = resultadoValidoCheckboxesSeleccionados(contarCheckboxesSeleccionados($checkboxes), minValores, maxValores, numeroExactoValores);
+      for (i = 0; i < checkboxes.length; i++){
+        e = checkboxes[i];
+        e.addEventListener('click', function(){
+          //Comprueba si el número de checkboxes seleccionados es correcto
+          resultadoValido = resultadoValidoCheckboxesSeleccionados(contarCheckboxesSeleccionados(checkboxes), minValores, maxValores, numeroExactoValores);
 
-        if ($campoError.length > 0){
-          $campoError[!resultadoValido ? 'show' : 'hide']();
-        }
+          if (campoError){
+            //campoError[!resultadoValido ? 'show' : 'hide']();
+          }
 
-        $checkboxes.each(function(){
-          $(this).get(0).setCustomValidity(resultadoValido ? '': mensajeError);
-        });
-      });
+          for (i = 0; i < checkboxes.length; i++){
+            e = checkboxes[i];
+            e.setCustomValidity(resultadoValido ? '': mensajeError);
+          }
+        });//jshint ignore:line
+      }
 
       //Establece la validez inicial sin mostrar errores
-      resultadoValido = resultadoValidoCheckboxesSeleccionados(contarCheckboxesSeleccionados($checkboxes), minValores, maxValores, numeroExactoValores);
+      resultadoValido = resultadoValidoCheckboxesSeleccionados(contarCheckboxesSeleccionados(checkboxes), minValores, maxValores, numeroExactoValores);
 
-      $checkboxes.each(function(){
-        $(this).get(0).setCustomValidity(resultadoValido ? '': mensajeError);
-      });
+      for (i = 0; i < checkboxes.length; i++){
+        e = checkboxes[i];
+        e.setCustomValidity(resultadoValido ? '': mensajeError);
+      }
     } else return;
   }
 
@@ -130,4 +135,4 @@
       validarCheckbox(campos[i]);
     }
   });
-})(window.jQuery);
+})();
